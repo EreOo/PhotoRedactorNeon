20398345347a2498a8faa8a79a812f6499fbb7b3
EreOo <Vladimircomment1995@gmail.com>
2018-04-11 23:31:25 +0300

    User can add photo add use filter VietnamWar. (TODO fix save button)

--- app/src/main/java/com/friendscompany/photoredactorneon/MainActivity.java
+++ app/src/main/java/com/friendscompany/photoredactorneon/MainActivity.java
@@ -1,15 +1,37 @@
 package com.friendscompany.photoredactorneon;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST = 1;
+    private static final int RESULT_LOAD_IMAGE = 0;
+    Button b_addPhoto, b_filter, b_save;
+    ImageView imageView, imageFilter;
+    String currentImage = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +40,116 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setPermission();
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageFilter = (ImageView) findViewById(R.id.imageFilter);
+        b_addPhoto = (Button) findViewById(R.id.addPhoto);
+        b_save = (Button) findViewById(R.id.save);
+        b_filter = (Button) findViewById(R.id.filter);
+
+        b_filter.setEnabled(false);
+        b_save.setEnabled(false);
+
+        b_addPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,RESULT_LOAD_IMAGE);
             }
         });
+
+        b_filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageFilter.setImageResource(R.drawable.ar);
+                b_save.setEnabled(true);
+            }
+        });
+
+        b_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            View content = findViewById(R.id.imageView);
+            Bitmap bitmap =getScreenShot(content);
+            currentImage = "image" + System.currentTimeMillis() + ".png";
+            store(bitmap,currentImage);
+            }
+        });
+    }
+
+    private static Bitmap getScreenShot(View view) {
+        view.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+        view.setDrawingCacheEnabled(false);
+        return bitmap;
+    }
+
+    private void store(Bitmap bitmap, String fileName) {
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/FILTEREDIMAGES";
+        File dir = new File(dirPath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File file = new File(dirPath, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+            Toast.makeText(this, "Saved.", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //    TODO: it may be better - but now work
+    private void setPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
+            }
+        } else {
+            // do nothing
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        // do nothing
+                    } else {
+                        Toast.makeText(this, "No Permission granted", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data){
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null,null,null);
+           cursor.moveToFirst();
+           int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+           String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            b_filter.setEnabled(true);
+        }
     }
 
     @Override
@@ -41,12 +165,10 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
